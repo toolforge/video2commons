3b9968c8fb5b3483a80274bf3ef4cd8dfea7f060
Zhuyifei1999 <zhuyifei1999@gmail.com>
2015-12-04 14:39:20 +0000

    Initial commit
    Added submodules and scripts for compiling avconv

---
+++ .profile
@@ -0,0 +1,5 @@
+alias ls='ls --color=tty'
+export PATH=~/.local/bin:$PATH
+export CPATH=~/.local/include
+export LIBRARY_PATH=~/.local/lib
+export PKG_CONFIG_PATH=~/.local/lib/pkgconfig
---
+++ converter-src/update.sh
@@ -0,0 +1,67 @@
+#!/bin/sh -ex
+echo "YASM"
+cd yasm
+git pull
+autoreconf -vif
+./configure --prefix=$HOME/.local
+make
+make install
+cd ..
+
+echo "OGG"
+cd libogg
+git pull
+autoreconf -vif
+./configure --prefix=$HOME/.local
+make
+make install
+cd ..
+
+
+echo "Theora"
+cd libtheora
+git pull
+autoreconf -vif
+./configure --prefix=$HOME/.local
+make
+make install
+cd ..
+
+echo "Vorbis"
+cd libvorbis
+git pull
+autoreconf -vif
+./configure --prefix=$HOME/.local
+make
+make install
+cd ..
+
+echo "Opus"
+cd libopus
+git pull
+autoreconf -vif
+./configure --prefix=$HOME/.local
+make
+make install
+cd ..
+
+echo "VPx"
+cd libvpx
+git pull
+autoreconf -vif || true
+./configure --prefix=$HOME/.local
+make
+make install
+cd ..
+
+echo "libav"
+cd libav
+git pull
+autoreconf -vif || true
+./configure --prefix=$HOME/.local --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libopus
+make
+make install
+cd ..
+
+
+echo "update.sh: done"
