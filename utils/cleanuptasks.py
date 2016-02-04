cda1beadac432b8dbca143020586b687b23570ac
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-02-04 20:38:35 +0800

    cleanuptasks: fix

--- utils/cleanuptasks.py
+++ utils/cleanuptasks.py
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 #
 
-import sys
+import os, sys
 from redis import Redis
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../")
