25a202c52e99d721548b9c2ba85c85d317370248
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-02-28 15:37:34 +0800

    upload: make pylint pass

--- backend/upload/__init__.py
+++ backend/upload/__init__.py
@@ -1,8 +1,6 @@
 #! /usr/bin/python
 # -*- coding: UTF-8 -*-
 #
-# Wrapper around pywikibot
-#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -17,18 +15,33 @@
 # along with self program.  If not, see <http://www.gnu.org/licenses/>
 #
 
+"""
+Uploads a file.
+
+Wrapper around pywikibot when the file can be uploaded automatically.
+If not, NeedServerSideUpload with url of file is thrown
+"""
+
 import os
 import shutil
 import pywikibot
 
+
 class NeedServerSideUpload(Exception):
+    """A server server-side is needed."""
+
     # So no one should handle it
     def __init__(self, url):
+        """Initialize the instance."""
         super(NeedServerSideUpload, self).__init__(url)
         self.url = url
 
-def upload(filename, wikifilename, sourceurl, http_host, filedesc, username,
-        statuscallback = None, errorcallback = None):
+
+def upload(
+    filename, wikifilename, sourceurl, http_host, filedesc, username,
+    statuscallback=None, errorcallback=None
+):
+    """Upload a file from filename to wikifilename."""
     statuscallback = statuscallback or (lambda text, percent: None)
     errorcallback = errorcallback or (lambda text: None)
 
@@ -46,22 +59,34 @@ def upload(filename, wikifilename, sourceurl, http_host, filedesc, username,
         chunked = (64 * (1 << 20)) if size >= 100000000 else 0
 
         statuscallback('Uploading...', -1)
-        if site.upload(page, source_filename=filename, comment=comment, text=filedesc, chunk_size=chunked, ignore_warnings=['exists-normalized']):
+        if site.upload(
+            page, source_filename=filename, comment=comment, text=filedesc,
+            chunk_size=chunked, ignore_warnings=['exists-normalized']
+        ):
             statuscallback('Upload success!', 100)
             return page.title(withNamespace=False), page.full_url()
         else:
             errorcallback('Upload failed!')
 
     else:
-        assert size < (1 << 32), 'Sorry, but files larger than 4GB can not be uploaded even with server-side uploading. This task may need manual intervention.'
+        assert size < (1 << 32), \
+            'Sorry, but files larger than 4GB can not be uploaded even ' + \
+            'with server-side uploading. This task may need manual ' + \
+            ' intervention.'
 
         # file name check
-        wikifilename = wikifilename.replace('/', '-').replace(' ', '_').replace('\r\n', '_').replace('\r', '_').replace('\n', '_')
+        wikifilename = wikifilename.replace('/', '-').replace(' ', '_')
+        wikifilename = wikifilename.replace('\r\n', '_')
+        wikifilename = wikifilename.replace('\r', '_').replace('\n', '_')
+
         newfilename = '/srv/v2c/ssu/' + wikifilename
         shutil.move(filename, newfilename)
 
         with open(newfilename + '.txt', 'w') as filedescfile:
-            filedesc = filedesc.replace('[[Category:Uploaded with video2commons]]', '[[Category:Uploaded with video2commons/Server-side uploads]]')
+            filedesc = filedesc.replace(
+                '[[Category:Uploaded with video2commons]]',
+                '[[Category:Uploaded with video2commons/Server-side uploads]]'
+            )
             filedescfile.write(filedesc.encode('utf-8'))
 
         fileurl = 'http://' + http_host + '/' + wikifilename
