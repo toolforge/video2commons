808c6ea67ff40c56dfb4be212f87e7eab74cdec4
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-05-20 21:18:13 +0800

    i18n: add the spaces back

--- www/python/src/static/lang/en.js
+++ www/python/src/static/lang/en.js
@@ -1,16 +1,16 @@
 var labels = {};
 //English language
 labels = {
-	remove: "Remove",
-	restart: "Restart",
-	next: "Next",
-	loading: "LOADING",
-	confirm: "Confirm ",
-	errorGeneric: "Something went terribly wrong. Please refresh this page or contact [[:commons:User:Zhuyifei1999]].",
-	errorTooLarge: "File too large to upload directly! You may want to <a>request a server-side upload</a>.",
-	addTask: "Add task",
-	yourTasks: "Your tasks",
-	createServerSide: "Create server-side upload ticket in one go (recommended)",
-	requestServerSide: "<a>request a server-side upload</a>",
-	taskDone: "Your task is done. You may find your upload at"
+	remove            :"Remove",
+	restart           :"Restart",
+	next              :"Next",
+	loading           :"LOADING",
+	confirm           :"Confirm ",
+	errorGeneric      :"Something went terribly wrong. Please refresh this page or contact [[:commons:User:Zhuyifei1999]].",
+	errorTooLarge     :"File too large to upload directly! You may want to <a>request a server-side upload</a>.",
+	addTask           :"Add task",
+	yourTasks         :"Your tasks",
+	createServerSide  :"Create server-side upload ticket in one go (recommended)",
+	requestServerSide :"<a>request a server-side upload</a>",
+	taskDone          :"Your task is done. You may find your upload at"
 };
