b2c2bd56030a7ce076bfed40d368c677a24446ce
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-12-23 16:45:37 +0800

    Make Gulp minify html

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require( 'gulp' );
 var uglify = require( 'gulp-uglify' );
 var rename = require( 'gulp-rename' );
+var htmlmin = require( 'gulp-htmlmin' );
 
 gulp.task( 'scripts', function() {
 	return gulp
@@ -11,12 +12,23 @@ gulp.task( 'scripts', function() {
 		.pipe( gulp.dest( './video2commons/frontend/static/' ) );
 } );
 
+gulp.task( 'html', function() {
+	return gulp
+		.src( [ './video2commons/frontend/**/*.html', '!./video2commons/frontend/**/*.min.html' ] )
+		.pipe( rename( { suffix: '.min' } ) )
+		.pipe( htmlmin( { collapseWhitespace: true, minifyCSS: true } ) )
+		.pipe( gulp.dest( './video2commons/frontend/' ) );
+} );
+
 gulp.task( 'watch', function() {
 	var changeevent = function( event ) {
-		console.log( 'File ' + event.path + ' was ' + event.type + ', running tasks...' );
+		console.log( 'File ' + event.path + ' was ' + event.type + ', running tasks...' );  // eslint-disable-line no-console
 	};
 	gulp.watch( [ './video2commons/frontend/static/*.js', '!./video2commons/frontend/static/*.min.js' ], [ 'scripts' ] )
 		.on( 'change', changeevent );
+
+	gulp.watch( [ './video2commons/frontend/**/*.html', '!./video2commons/frontend/**/*.min.html' ], [ 'html' ] )
+		.on( 'change', changeevent );
 } );
 
-gulp.task( 'default', [ 'scripts', 'watch' ] );
+gulp.task( 'default', [ 'scripts', 'html', 'watch' ] );
