413de0a3e66d3fb583b6430eddf6dd6eeca77237
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-12-24 10:59:55 +0800

    Upload: Increase Chunk Size to 16MB
    This shall reduce the number of chunks.

--- video2commons/backend/upload/__init__.py
+++ video2commons/backend/upload/__init__.py
@@ -91,7 +91,7 @@ def upload_pwb(
         errorcallback('File already exists. Please choose another name.')
 
     comment = u'Imported media from ' + sourceurl
-    chunked = (4 * (1 << 20)) if size >= 100000000 else 0
+    chunked = (16 * (1 << 20)) if size >= 100000000 else 0
 
     statuscallback('Uploading...', -1)
     try:
