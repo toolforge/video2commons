05ec6de55f563e0c21f230755ed9a3bb183001f6
Roy Zuo <26777013+royjor2016@users.noreply.github.com>
2023-07-05 23:42:32 +0200

    add [[Category:Videos of %(yyyy)s|%(mm_dd)s]]

--- video2commons/frontend/urlextract.py
+++ video2commons/frontend/urlextract.py
@@ -50,6 +50,7 @@ FILEDESC_TEMPLATE = """
 {{LicenseReview}}
 
 [[Category:Uploaded with video2commons]]
+[[Category:Videos of %(yyyy)s|%(mm_dd)s]]
 """
 
 
@@ -102,7 +103,9 @@ def do_extract_url(url):
         'date': _date(url, ie_key, title, info),
         'source': _source(url, ie_key, title, info),
         'uploader': _uploader(url, ie_key, title, info),
-        'license': _license(url, ie_key, title, info)
+        'license': _license(url, ie_key, title, info),
+        'yyyy': _date(url, ie_key, title, info)[1],
+        'mm_dd': _date(url, ie_key, title, info)[2]
     }
 
     return {
@@ -115,9 +118,15 @@ def do_extract_url(url):
 
 def _date(url, ie_key, title, info):
     date = (info.get('upload_date') or '').strip()
+    yyyy = ''
+    mm_dd = ''
+
     if re.match(r'^[0-9]{8}$', date):
+        yyyy = date[0:4]
+        mm_dd = f"{date[4:6]}-{date[6:8]}"
         date = '%s-%s-%s' % (date[0:4], date[4:6], date[6:8])
-    return date
+
+    return date, yyyy, mm_dd
 
 
 def _source(url, ie_key, title, info):
