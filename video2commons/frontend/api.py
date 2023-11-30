bc149972ddccd7ea2a7f8b9650d16001e0e5ee3c
Roy Zuo <26777013+royjor2016@users.noreply.github.com>
2023-11-30 16:15:38 +0100

    bug 173

--- video2commons/frontend/api.py
+++ video2commons/frontend/api.py
@@ -335,14 +335,15 @@ def get_backend_keys(format):
 
     MAXSIZE = 4 << 30
     COMBINED_FMT = (
-        'bestvideo[vcodec={{vcodec}}][filesize<{max}]+'
+#        'bestvideo[vcodec={{vcodec}}][filesize<{max}]+' bug 173, commented out, until an expert write something better
+        'bestvideo[ext={{vext}}][filesize<{max}]+'
         'bestaudio[acodec={{acodec}}]/'
         'bestvideo[ext={{vext}}][filesize<{max}]+'
         'bestaudio[ext={{aext}}]/'
         'bestvideo+bestaudio/best'
     ).format(max=MAXSIZE)
     VIDEO_FMT = (
-        'bestvideo[vcodec={{vcodec}}][filesize<{max}]/'
+#        'bestvideo[vcodec={{vcodec}}][filesize<{max}]/' bug 173 temp fix
         'bestvideo[ext={{vext}}][filesize<{max}]/'
         'bestvideo/best'
     ).format(max=MAXSIZE)
