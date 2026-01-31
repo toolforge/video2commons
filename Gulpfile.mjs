96ea1f3e73ac8a9027b73ccdd11dac4ba69fcd27
Shun Sakai <sorairolake@protonmail.ch>
2026-01-31 14:06:20 +0900

    ci: Add Biome as code formatter and linter

--- Gulpfile.mjs
+++ Gulpfile.mjs
@@ -1,52 +1,74 @@
-import gulp from 'gulp';
-import concat from 'gulp-concat';
-import htmlmin from 'gulp-htmlmin';
-import rename from 'gulp-rename';
-import uglify from 'gulp-uglify';
-import {nunjucksPrecompile} from 'gulp-nunjucks';
-/* eslint-env node */
+import gulp from "gulp";
+import concat from "gulp-concat";
+import htmlmin from "gulp-htmlmin";
+import { nunjucksPrecompile } from "gulp-nunjucks";
+import rename from "gulp-rename";
+import uglify from "gulp-uglify";
 
-gulp.task( 'scripts', function () {
-	return gulp
-		.src( [ './video2commons/frontend/static/*.js', '!./video2commons/frontend/static/*.min.js' ] )
-		.pipe( rename( { suffix: '.min' } ) )
-		.pipe( uglify() )
-		.pipe( gulp.dest( './video2commons/frontend/static/' ) );
-} );
+gulp.task("scripts", () =>
+	gulp
+		.src([
+			"./video2commons/frontend/static/*.js",
+			"!./video2commons/frontend/static/*.min.js",
+		])
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(uglify())
+		.pipe(gulp.dest("./video2commons/frontend/static/")),
+);
 
-gulp.task( 'jinja2', function () {
-	return gulp
-		.src( [ './video2commons/frontend/templates/**.html', '!./video2commons/frontend/templates/**.min.html' ] )
-		.pipe( rename( { suffix: '.min' } ) )
-		.pipe( htmlmin( { collapseWhitespace: true, minifyCSS: true } ) )
-		.pipe( gulp.dest( './video2commons/frontend/templates/' ) );
-} );
+gulp.task("jinja2", () =>
+	gulp
+		.src([
+			"./video2commons/frontend/templates/**.html",
+			"!./video2commons/frontend/templates/**.min.html",
+		])
+		.pipe(rename({ suffix: ".min" }))
+		.pipe(htmlmin({ collapseWhitespace: true, minifyCSS: true }))
+		.pipe(gulp.dest("./video2commons/frontend/templates/")),
+);
 
-gulp.task( 'nunjucks', function () {
-	return gulp
-		.src( [ './video2commons/frontend/static/templates/**.html' ] )
-		.pipe( htmlmin( { collapseWhitespace: true, minifyCSS: true } ) )
-		.pipe( nunjucksPrecompile() )
-		.pipe( concat( '../templates.min.js' ) )
-		.pipe( uglify() )
-		.pipe( gulp.dest( './video2commons/frontend/static/templates' ) );
-} );
+gulp.task("nunjucks", () =>
+	gulp
+		.src(["./video2commons/frontend/static/templates/**.html"])
+		.pipe(htmlmin({ collapseWhitespace: true, minifyCSS: true }))
+		.pipe(nunjucksPrecompile())
+		.pipe(concat("../templates.min.js"))
+		.pipe(uglify())
+		.pipe(gulp.dest("./video2commons/frontend/static/templates")),
+);
 
-gulp.task( 'watch', function () {
-	var changeevent = function ( event ) {
-		// eslint-disable-next-line no-console
-		console.log( 'File ' + event.path + ' was ' + event.type + ', running tasks...' );
+gulp.task("watch", () => {
+	var changeevent = (event) => {
+		console.log(`File ${event.path} was ${event.type}, running tasks...`);
 	};
-	gulp.watch( [ './video2commons/frontend/static/*.js', '!./video2commons/frontend/static/*.min.js' ],  gulp.series('scripts') )
-		.on( 'change', changeevent );
+	gulp
+		.watch(
+			[
+				"./video2commons/frontend/static/*.js",
+				"!./video2commons/frontend/static/*.min.js",
+			],
+			gulp.series("scripts"),
+		)
+		.on("change", changeevent);
 
-	gulp.watch( [ './video2commons/frontend/templates/**.html', '!./video2commons/frontend/templates/**.min.html' ], gulp.series('jinja2') )
-		.on( 'change', changeevent );
+	gulp
+		.watch(
+			[
+				"./video2commons/frontend/templates/**.html",
+				"!./video2commons/frontend/templates/**.min.html",
+			],
+			gulp.series("jinja2"),
+		)
+		.on("change", changeevent);
 
-	gulp.watch( [ './video2commons/frontend/static/templates/**.html' ], gulp.series('nunjucks') )
-		.on( 'change', changeevent );
-} );
+	gulp
+		.watch(
+			["./video2commons/frontend/static/templates/**.html"],
+			gulp.series("nunjucks"),
+		)
+		.on("change", changeevent);
+});
 
-gulp.task( 'build', gulp.series( 'scripts', 'jinja2', 'nunjucks' ));
+gulp.task("build", gulp.series("scripts", "jinja2", "nunjucks"));
 
-gulp.task( 'default', gulp.series( 'build', 'watch' ));
+gulp.task("default", gulp.series("build", "watch"));
